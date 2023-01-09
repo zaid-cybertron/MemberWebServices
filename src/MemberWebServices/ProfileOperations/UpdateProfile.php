15a809d2a4b7274f6272e507813d8a966c3bbc44
zaid-cybertron <zaid@cybertrontechnologies.com>
2023-01-09 13:35:00 +0530

    Added User Profile update feature

---
+++ src/MemberWebServices/ProfileOperations/UpdateProfile.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace MemberWebServices\ProfileOperations;
+use MemberWebServices\Config;
+use MemberWebServices\CallApi;
+
+class UpdateProfile
+{
+    private $callApiObj;
+    private $body = [];
+    private $REQUESTTYPE = "PUT";
+    private $URLPARAMS;
+    private $CONTENTTYPE = "application/json";
+    private $options = [];
+    private $response;
+    private $headers = [];
+    private $programUdk;
+    private $bodyParameters = [];
+
+    public function __construct($memberAccessToken,$memberAccountNumber,$bodyParameters)
+    {
+        $this->headers = [
+            "content-type" => $this->CONTENTTYPE,
+            "memberAccessToken" => $memberAccessToken,
+        ];
+        $config = new Config();
+        $this->programUdk = $config->getProgramId();
+        
+        $this->URLPARAMS = "member-connect.excentus.com/fuelrewards/public/rest/v2/" . $this->programUdk . "/members/$memberAccountNumber"; 
+    
+        $this->body = [
+            $this->bodyParameters,
+        ];
+
+        $this->callApiObj = new CallApi($this->REQUESTTYPE,$this->URLPARAMS,$this->headers,$this->options,$this->body);
+        $this->response = $this->callApiObj->requestApi();
+    }
+    public function getResponse(){
+        return json_decode($this->response->getBody()->getContents());
+    }
+
+}
+
+?>

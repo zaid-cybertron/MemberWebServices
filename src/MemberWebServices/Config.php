853205009e9e0eb9f37f344832a6cbb0b2437988
zaid-cybertron <zaid@cybertrontechnologies.com>
2023-03-09 12:53:40 +0530

    Added app env in config class

--- src/MemberWebServices/Config.php
+++ src/MemberWebServices/Config.php
@@ -19,7 +19,8 @@ class Config{
     public function __construct(){
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->safeLoad();
-        $this->baseUrl = $_ENV['BASE_URL'];
+        $appEnv = $_ENV('APP_ENV');
+        $this->baseUrl = $appEnv == 'local' ? $_ENV['STAGE_URL'] : $_ENV['BASE_URL'];
         $this->clientUserIdDev = $_ENV['CLIENT_USER_ID_DEV'];
         $this->clientUserIdProd = $_ENV['CLINET_USER_ID_PROD'];
         $this->clientUserSecreteDev = $_ENV['CLIENT_USER_SECRET_DEV'];
